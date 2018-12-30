550c311e8aa28d1101f3eb826370fe6003cb3a07
SQLatEnwiki <sqlatenwiki@gmail.com>
2018-12-30 17:58:04 -0500

    Add examples, simplify eureka a bit

--- home.php
+++ home.php
@@ -1,13 +1,18 @@
 <?php
 
 function getEurekaInfo( $host ) {
-	$curl = curl_init();
-	curl_setopt( $curl, CURLOPT_URL, "$host:8008/setup/eureka_info" );
-	curl_setopt( $curl, CURLOPT_RETURNTRANSFER, 1 );
-	$ret = curl_exec( $curl );
+	$url = "$host:8008/setup/eureka_info";
+	$ret = remotePull( $url );
 	if( $ret === FALSE ) { die( "Failure to retrieve EurekaInfo\n" . curl_error( $curl ) ); }
 	$res = json_decode( $ret, TRUE );
 	return( $res );
 }
 
+function remotePull( $url ) {
+	$curl = curl_init();
+	curl_setopt( $curl, CURLOPT_URL, $url );
+	curl_setopt( $curl, CURLOPT_RETURNTRANSFER, 1 );
+	$ret = curl_exec( $curl );
+	return( $ret );
+}
 ?>
