bb5c9b2a0947faf171b7109ef360bb2645a63eef
SQLatEnwiki <sqlatenwiki@gmail.com>
2018-12-30 18:07:45 -0500

    Added alarm handling

--- home.php
+++ home.php
@@ -15,4 +15,17 @@ function remotePull( $url ) {
 	$ret = curl_exec( $curl );
 	return( $ret );
 }
+
+function getAlarms ( $host ) {
+	$url = "$host:8008/setup/assistant/alarms";
+	$ret = remotePull( $url );
+	if( $ret === FALSE ) { die( "Failure to retrieve EurekaInfo\n" . curl_error( $curl ) ); }
+	$alarms = json_decode( $ret, TRUE );
+	if( count( $alarms['alarm'] ) == 0 ) { return( FALSE ); }
+	$ret = array();
+	foreach( $alarms['alarm'] as $alarm ) {
+		array_push( $ret, $alarm );
+	}
+	return( $ret );
+}
 ?>
